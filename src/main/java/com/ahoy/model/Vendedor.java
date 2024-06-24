3cf96eea3b5afdf0d8d312ac4cc9823c03f23274
Ricardo Machado Poffo <tiorica@gmail.com>
2024-06-24 19:36:38 -0300

    test: teste create vendedor

--- src/main/java/com/ahoy/model/Vendedor.java
+++ src/main/java/com/ahoy/model/Vendedor.java
@@ -42,9 +42,8 @@ public class Vendedor {
     @CNPJ(groups = PessoaJuridica.class, message = "CNPJ inválido")
     private String documento;
 
-    @NotBlank
     @Email(message = "Email deve ser válido")
-    @Pattern(regexp = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$", message = "Email deve ser válido")
+    // @Pattern(regexp = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$", message = "Email deve ser válido")
     private String email;
 
     @Enumerated(EnumType.STRING)
